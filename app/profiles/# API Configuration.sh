d80550fdd73291b696093bd785e4472b05b67d60
Blessing Mwale <blessingmwalein@gmail.com>
2025-12-16 12:27:50 +0200

    local

---
+++ app/profiles/# API Configuration.sh
@@ -0,0 +1,20 @@
+# API Configuration
+NEXT_PUBLIC_API_BASE_URL=https://ubiqent.globalfortuneint.com/api
+NEXT_PUBLIC_ADMIN_BASE_URL=https://ubiqent.globalfortuneint.com/admin
+
+# NEXT_PUBLIC_API_BASE_URL=http://127.0.0.1/api
+# NEXT_PUBLIC_ADMIN_BASE_URL=https://ubiqent.com/admin
+
+# Stripe Configuration
+NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY=pk_test_your_publishable_key
+
+# App Configuration
+NEXT_PUBLIC_APP_NAME=UBIQ Entertainment
+NEXT_PUBLIC_APP_URL=http://localhost:3000
+
+# Video Configuration  
+NEXT_PUBLIC_VIDEO_BASE_URL=https://ubiqent.globalfortuneint.com/storage/videos
+NEXT_PUBLIC_IMAGE_BASE_URL=https://ubiqent.globalfortuneint.com
+
+# NEXT_PUBLIC_VIDEO_BASE_URL=http://127.0.0.1/storage/videos
+# NEXT_PUBLIC_IMAGE_BASE_URL=http://127.0.0.1/
